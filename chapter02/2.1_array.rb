13a916c3bda24d509eecc08486cbd76f29e4f433
ahakits <ahakits1987@gmail.com>
2017-06-11 19:20:02 +0900

    add Chapter02

---
+++ chapter02/2.1_array.rb
@@ -0,0 +1,9 @@
+names = ['Alice', 'Bob', 'Carol', 'Dave']
+print 'The first name is', names[0], "\n"
+
+mixed = [0, 'Bob', 2, 'Carol', 4]
+puts mixed.size
+
+names.each do |n|
+  puts n
+end
---
+++ chapter02/2.3_regular_expression.rb
@@ -0,0 +1,6 @@
+/Ruby/ =~'ruby'
+/Ruby/ =~ 'RUBY'
+/Ruby/i =~ 'ruby'
+/Ruby/i =~ 'RUBY'
+/Ruby/i =~ 'rUbY'
+/Ruby/ =~ 'Yet Another Ruby Hacker,'
