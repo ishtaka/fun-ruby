91df5c42d10b57ea536453ea341e8cfea71060bb
ahakits <ahakits1987@gmail.com>
2017-06-24 22:28:08 +0900

    add Chapter13

---
+++ chapter13/13.1_list.rb
@@ -0,0 +1,4 @@
+list = %w[a b c d]
+for i in 0..3
+  print i + 1, '番目の要素は', list[i], "です。\n"
+end
---
+++ chapter13/13.4_list.rb
@@ -0,0 +1,5 @@
+list = %w[a b c d]
+
+list.each_with_index do |elem, i|
+  puts "#{i + 1}番目の要素は#{elem}です。"
+end
